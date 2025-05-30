b7cdc556dd5bb7b21e9eef47cee13dc5e2668819
alex-yi-writes <57142280+alex-yi-writes@users.noreply.github.com>
2025-05-30 11:05:30 +0200

    Add files via upload

--- layer_analysis/ENGRAMS_compcor.py
+++ layer_analysis/ENGRAMS_compcor.py
@@ -9,16 +9,72 @@ for x in ID:
 
     id = x
 
-    fmri        = "v1s1/func/au" + id + "v1s1_task-origenc_run-03_bold.nii"
-    mask        = "v1s1/func/origenc_mask.nii.gz"
-    csfmask     = "v1s1/func/origenc_csf_bin.nii"
-    wmmask      = "v1s1/anat/roi/func/origenc/WM_clean.nii.gz"
+    print ("running "+id)
+
+    print("resting state")
+
+    fmri        = "v1s1/func/au" + id + "v1s1_task-rest_run-01_bold.nii"
+    mask        = "v1s1/func/rest_mask.nii.gz"
+    csfmask     = "v1s1/func/rest_csf_bin.nii"
+    wmmask      = "v1s1/anat/roi/func/rest/WM_clean.nii.gz"
 
     fmripath    = parpath + id + fmri
     csfpath     = parpath + id + csfmask
     wmpath      = parpath + id + wmmask
     # maskpath   = parpath + id + mask # wb mask needs too much memory for any of my machine to handle
-    outputpath  = parpath + id + "v1s1/func/compcor_csf_wm_origenc.txt"
+    outputpath  = parpath + id + "v1s1/func/compcor_csf_wm_rest.txt"
+
+    ccinterface = CompCor()
+    ccinterface.inputs.realigned_file       = fmripath
+    ccinterface.inputs.mask_files           = [csfpath, wmpath] # always wrap it
+    ccinterface.inputs.merge_method         = 'none'
+    ccinterface.inputs.num_components       = 3
+    ccinterface.inputs.pre_filter           = 'polynomial'
+    ccinterface.inputs.regress_poly_degree  = 2
+    ccinterface.inputs.repetition_time      = TR
+    ccinterface.inputs.components_file      = outputpath
+    ccinterface.run()
+    
+    
+
+    # print("original encoding")
+
+    # fmri        = "v1s1/func/au" + id + "v1s1_task-origenc_run-03_bold.nii"
+    # mask        = "v1s1/func/origenc_mask.nii.gz"
+    # csfmask     = "v1s1/func/origenc_csf_bin.nii"
+    # wmmask      = "v1s1/anat/roi/func/origenc/WM_clean.nii.gz"
+
+    # fmripath    = parpath + id + fmri
+    # csfpath     = parpath + id + csfmask
+    # wmpath      = parpath + id + wmmask
+    # # maskpath   = parpath + id + mask # wb mask needs too much memory for any of my machine to handle
+    # outputpath  = parpath + id + "v1s1/func/compcor_csf_wm_origenc.txt"
+
+    # ccinterface = CompCor()
+    # ccinterface.inputs.realigned_file       = fmripath
+    # ccinterface.inputs.mask_files           = [csfpath, wmpath] # always wrap it
+    # ccinterface.inputs.merge_method         = 'none'
+    # ccinterface.inputs.num_components       = 3
+    # ccinterface.inputs.pre_filter           = 'polynomial'
+    # ccinterface.inputs.regress_poly_degree  = 2
+    # ccinterface.inputs.repetition_time      = TR
+    # ccinterface.inputs.components_file      = outputpath
+    # ccinterface.run()
+
+
+
+    print("original recognition")
+
+    fmri        = "v1s1/func/au" + id + "v1s1_task-origrec_run-04_bold.nii"
+    mask        = "v1s1/func/origrec_mask.nii.gz"
+    csfmask     = "v1s1/func/origrec_csf_bin.nii"
+    wmmask      = "v1s1/anat/roi/func/origrec/WM_clean.nii.gz"
+
+    fmripath    = parpath + id + fmri
+    csfpath     = parpath + id + csfmask
+    wmpath      = parpath + id + wmmask
+    # maskpath   = parpath + id + mask # wb mask needs too much memory for any of my machine to handle
+    outputpath  = parpath + id + "v1s1/func/compcor_csf_wm_orirec.txt"
 
     ccinterface = CompCor()
     ccinterface.inputs.realigned_file       = fmripath

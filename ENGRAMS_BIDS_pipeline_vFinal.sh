85b3b579db9cf40b980ab69b54d25d8cfb3611bd
alex-yi-writes <57142280+alex-yi-writes@users.noreply.github.com>
2025-06-11 13:33:42 +0200

    Add files via upload

--- ENGRAMS_BIDS_pipeline_vFinal.sh
+++ ENGRAMS_BIDS_pipeline_vFinal.sh
@@ -4,8 +4,8 @@
 raw_data_dir="/Users/yyi/Desktop/ENGRAMS/raw"    # Directory containing subject data
 extracted_dir="/Users/yyi/Desktop/ENGRAMS/preproc/tmp"   # Temporary directory for processing
 output_dir="/Users/yyi/Desktop/ENGRAMS/preproc"          # Target BIDS directory
-config_file="/Users/yyi/Desktop/ENGRAMS/scripts/ENGRAMS_dcm2bids_new.json" # dcm2bids config file
-subject_ids=("108v2s2") #("101v1s1" "101v1s2" "101v2s1" "101v2s2" "102v1s1" "102v1s2" "102v2s1" "102v2s2" "103v1s1" "103v1s2" "103v2s1" "103v2s2")                  # List of subject IDs
+config_file="/Users/yyi/Desktop/ENGRAMS/scripts/ENGRAMS_dcm2bids_vf.json" # dcm2bids config file
+subject_ids=("205v1s1" "205v1s2") #("101v1s1" "101v1s2" "101v2s1" "101v2s2" "102v1s1" "102v1s2" "102v2s1" "102v2s2" "103v1s1" "103v1s2" "103v2s1" "103v2s2")                  # List of subject IDs
 
 # loop over ids
 for subject in "${subject_ids[@]}"; do

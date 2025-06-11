f876c33fb5e06e1c68f856f0dd4e77a45edc0e28
alex-yi-writes <57142280+alex-yi-writes@users.noreply.github.com>
2025-06-11 13:34:35 +0200

    Add files via upload

--- fmri_analysis/ENGRAMS_distortionCorrect_fMRI_Day1.sh
+++ fmri_analysis/ENGRAMS_distortionCorrect_fMRI_Day1.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:/usr/local/fsl/bin
 source $FSLDIR/etc/fslconf/fsl.sh
 
 # subject
-SUBJECT="sub-201"   # TODO: update list
+SUBJECT="sub-205"   # TODO: update list
 echo "$SUBJECT"
 
 ##########################################################################
@@ -19,7 +19,7 @@ OUTPUT_DIR="/Users/yyi/Desktop/ENGRAMS/preproc/${SUBJECT}v1s1/func"
 ####### ===== STEP 1: extract single-volume references (AP & PA) ===== #######
 echo "extracting single-volume BOLD references"
 fslroi "$DATA_DIR/${SUBJECT}v1s1_task-rest_run-01_bold.nii.gz" "$OUTPUT_DIR/${SUBJECT}v1s1_rest_AP_b0.nii.gz" 0 1
-fslroi "$DATA_DIR/${SUBJECT}v1s1_antiPE_bold.nii.gz" "$OUTPUT_DIR/${SUBJECT}v1s1_PA_b0.nii.gz" 0 1
+fslroi "$DATA_DIR/${SUBJECT}v1s1_run-01_antiPE_bold.nii.gz" "$OUTPUT_DIR/${SUBJECT}v1s1_PA_b0.nii.gz" 0 1
 
 for f in  "${SUBJECT}v1s1_rest_AP_b0.nii.gz" "${SUBJECT}v1s1_PA_b0.nii.gz"; do
     fslreorient2std "$OUTPUT_DIR/$f" "${OUTPUT_DIR}/${f%.nii.gz}_std.nii.gz"
@@ -63,3 +63,85 @@ applytopup --imain="$DATA_DIR/${SUBJECT}v1s1_task-rest_run-01_bold.nii.gz" \
            --out="$OUTPUT_DIR/${SUBJECT}v1s1_task-rest_run-01_bold_topup"
 
 echo "resting state processing done"
+
+
+
+##########################################################################
+#          TOPUP-based susceptibility correction for ORIGENC             #
+##########################################################################
+
+# paths
+DATA_DIR="/Users/yyi/Desktop/ENGRAMS/preproc/${SUBJECT}v1s1/func"
+OUTPUT_DIR="/Users/yyi/Desktop/ENGRAMS/preproc/${SUBJECT}v1s1/func"
+
+####### ===== STEP 1: extract single-volume references (AP & PA) ===== #######
+echo "extracting single-volume BOLD references"
+fslroi "$DATA_DIR/${SUBJECT}v1s1_task-origenc_run-03_bold.nii.gz" "$OUTPUT_DIR/${SUBJECT}v1s1_origenc_AP_b0.nii.gz" 0 1
+
+####### ===== STEP 2: merge references ===== #######
+echo "merging references"
+fslmerge -t "$OUTPUT_DIR/${SUBJECT}v1s1_origenc_b0_AP_PA.nii.gz" \
+         "$OUTPUT_DIR/${SUBJECT}v1s1_origenc_AP_b0.nii.gz" \
+         "$OUTPUT_DIR/${SUBJECT}v1s1_PA_b0.nii.gz"
+
+####### ===== STEP 4: run TOPUP ===== #######
+echo "running TOPUP"
+topup --imain="$OUTPUT_DIR/${SUBJECT}v1s1_origenc_b0_AP_PA.nii.gz" \
+      --datain="$OUTPUT_DIR/acqparams.txt" \
+      --config=b02b0.cnf \
+      --out="$OUTPUT_DIR/${SUBJECT}v1s1_origenc_topup_results" \
+      --fout="$OUTPUT_DIR/${SUBJECT}v1s1_origenc_fieldmap_Hz.nii.gz" \
+      --iout="$OUTPUT_DIR/${SUBJECT}v1s1_origenc_unwarped_b0.nii.gz"
+echo "TOPUP done"
+
+####### ===== STEP 5: apply correction to full BOLD runs ===== #######
+echo "applying TOPUP to full timeseries"
+applytopup --imain="$DATA_DIR/${SUBJECT}v1s1_task-origenc_run-03_bold.nii.gz" \
+           --datain="$OUTPUT_DIR/acqparams.txt" \
+           --inindex=2 \
+           --topup="$OUTPUT_DIR/${SUBJECT}v1s1_origenc_topup_results" \
+           --method=jac \
+           --out="$OUTPUT_DIR/${SUBJECT}v1s1_task-origenc_run-03_bold_topup"
+
+echo "encoding (original) done"
+
+
+
+##########################################################################
+#          TOPUP-based susceptibility correction for ORIGREC             #
+##########################################################################
+
+# paths
+DATA_DIR="/Users/yyi/Desktop/ENGRAMS/preproc/${SUBJECT}v1s1/func"
+OUTPUT_DIR="/Users/yyi/Desktop/ENGRAMS/preproc/${SUBJECT}v1s1/func"
+
+####### ===== STEP 1: extract single-volume references (AP & PA) ===== #######
+echo "extracting single-volume BOLD references"
+fslroi "$DATA_DIR/${SUBJECT}v1s1_task-origrec_run-03_bold.nii.gz" "$OUTPUT_DIR/${SUBJECT}v1s1_origrec_AP_b0.nii.gz" 0 1
+
+####### ===== STEP 2: merge references ===== #######
+echo "merging references"
+fslmerge -t "$OUTPUT_DIR/${SUBJECT}v1s1_origrec_b0_AP_PA.nii.gz" \
+         "$OUTPUT_DIR/${SUBJECT}v1s1_origrec_AP_b0.nii.gz" \
+         "$OUTPUT_DIR/${SUBJECT}v1s1_PA_b0.nii.gz"
+
+####### ===== STEP 4: run TOPUP ===== #######
+echo "running TOPUP"
+topup --imain="$OUTPUT_DIR/${SUBJECT}v1s1_origrec_b0_AP_PA.nii.gz" \
+      --datain="$OUTPUT_DIR/acqparams.txt" \
+      --config=b02b0.cnf \
+      --out="$OUTPUT_DIR/${SUBJECT}v1s1_origrec_topup_results" \
+      --fout="$OUTPUT_DIR/${SUBJECT}v1s1_origrec_fieldmap_Hz.nii.gz" \
+      --iout="$OUTPUT_DIR/${SUBJECT}v1s1_origrec_unwarped_b0.nii.gz"
+echo "TOPUP done"
+
+####### ===== STEP 5: apply correction to full BOLD runs ===== #######
+echo "applying TOPUP to full timeseries"
+applytopup --imain="$DATA_DIR/${SUBJECT}v1s1_task-origrec_run-03_bold.nii.gz" \
+           --datain="$OUTPUT_DIR/acqparams.txt" \
+           --inindex=2 \
+           --topup="$OUTPUT_DIR/${SUBJECT}v1s1_origrec_topup_results" \
+           --method=jac \
+           --out="$OUTPUT_DIR/${SUBJECT}v1s1_task-origrec1_run-03_bold_topup"
+
+echo "recognition (original) done"

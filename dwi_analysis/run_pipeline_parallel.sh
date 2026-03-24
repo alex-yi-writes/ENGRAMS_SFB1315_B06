caeacad23b63d30493d7427916118f1a69e65bcd
Yeo-Jin Yi <57142280+alex-yi-writes@users.noreply.github.com>
2026-03-24 14:45:00 +0100

    Update run_pipeline_parallel.sh
    removed placeholder... no reason to put anything else for now

--- dwi_analysis/run_pipeline_parallel.sh
+++ dwi_analysis/run_pipeline_parallel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# generate commands with temporal_phase=1 for experimentation
-./generate_parallel_commands.sh r
+# generate commands with temporal_phase=3 for experimentation
+./generate_parallel_commands.sh 3
 
 # then run with GNU parallel
 parallel --jobs 4 --delay 300 --joblog full_log.txt < full_pipeline_commands.txt

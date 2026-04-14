e855e89b6c349278c3d96feb011eed9454747d2e
Yeo-Jin Yi <57142280+alex-yi-writes@users.noreply.github.com>
2026-04-14 15:57:19 +0200

    Update and rename ENGRAMS_WBseg_reconall_102.sh to ENGRAMS_WBseg_reconall.sh

--- layer_analysis/ENGRAMS_WBseg_reconall.sh
+++ layer_analysis/ENGRAMS_WBseg_reconall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ID in sub-102v1s1
+for ID in sub-301v1s1
 do
 	path_src=/mnt/work/yyi/ENGRAMS/preproc/${ID}/anat/t1
 	path_out=/mnt/work/yyi/ENGRAMS/preproc/${ID}/anat/t1/${ID}/mri
@@ -9,13 +9,10 @@ do
 	export FREESURFER_HOME=/share/apps/freesurfer_7.1  
 	source $FREESURFER_HOME/SetUpFreeSurfer.sh
 	export ANTSPATH=/share/apps/ants/
+	export PATH=$ANTSPATH:$PATH
 	export SUBJECTS_DIR=$path_src
 	
-	#mkdir /mnt/work/yyi/ENGRAMS/preproc/${ID}/anat/t1/${ID}
-
 	which recon-all
-
-	export SUBJECTS_DIR=$path_src
 
 	# stage 1
 	recon-all \
@@ -26,11 +23,11 @@ do
 		-autorecon1 \
 		-noskullstrip
 	mri_convert -it mgz -ot nii ${path_out}/orig.mgz ${path_src}/orig.nii.gz
-	/share/apps/ants/antsRegistrationSyNQuick.sh -d 3 -t r -f ${path_src}/orig.nii.gz -m ${t1w} -o ${path_src}/Nat2FS_
-	#/share/apps/ants/antsRegistrationSyNQuick.sh -d 3 -t r -m ${path_src}/orig.nii.gz -f ${t1w} -o ${path_src}/FS2Nat_
+	antsRegistrationSyNQuick.sh -d 3 -t r -m ${path_src}/orig.nii.gz -f ${t1w} -o ${path_src}/FS2Nat_
+	antsRegistrationSyNQuick.sh -d 3 -t r -m ${path_src}/orig.nii.gz -f ${t1w} -o ${path_src}/Nat2FS_
 	
 	# stage 2
-	/share/apps/ants/antsApplyTransforms -d 3 -n Linear -i ${path_src}/brainmask.nii -r ${path_src}/orig.nii.gz -o ${path_out}/brainmask.nii.gz -t ${path_src}/Nat2FS_0GenericAffine.mat
+	antsApplyTransforms -d 3 -n Linear -i ${path_src}/brainmask.nii -r ${path_src}/orig.nii.gz -o ${path_out}/brainmask.nii.gz -t ${path_src}/Nat2FS_0GenericAffine.mat
 	 mri_convert -it nii -ot mgz ${path_out}/brainmask.nii.gz ${path_out}/brainmask.mgz
 
 	 recon-all -s ${ID} \

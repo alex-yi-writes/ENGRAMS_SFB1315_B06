617d41706a7c135401cc58f91e3fad7f70e2259b
Yeo-Jin Yi <57142280+alex-yi-writes@users.noreply.github.com>
2026-04-16 12:25:43 +0200

    Add files via upload

--- layer_analysis/final_workflow/A3-e_ENGRAMS_skullstrip_EXPERIMENT.sh
+++ layer_analysis/final_workflow/A3-e_ENGRAMS_skullstrip_EXPERIMENT.sh
@@ -9,6 +9,19 @@ do
 
 	# DAY 1
 	mri_synthstrip \
+	-i /Users/yyi/Desktop/ENGRAMS/preproc/sub-"${ID}"v1s1/func/meansub-"${ID}"v1s1_task-rest_run-01_bold_topup.nii.gz \
+	-o /Users/yyi/Desktop/ENGRAMS/preproc/sub-"${ID}"v1s1/func/meansub-"${ID}"v1s1_task-rest_run-01_bold_topup_stripped.nii.gz
+
+	mri_synthstrip \
+	-i /Users/yyi/Desktop/ENGRAMS/preproc/sub-"${ID}"v1s1/func/meansub-"${ID}"v1s1_task-origenc_run-03_bold_topup.nii.gz \
+	-o /Users/yyi/Desktop/ENGRAMS/preproc/sub-"${ID}"v1s1/func/meansub-"${ID}"v1s1_task-origenc_run-03_bold_topup_stripped.nii.gz
+
+	mri_synthstrip \
+	-i /Users/yyi/Desktop/ENGRAMS/preproc/sub-"${ID}"v1s1/func/meansub-"${ID}"v1s1_task-origrec1_run-04_bold_topup.nii.gz \
+	-o /Users/yyi/Desktop/ENGRAMS/preproc/sub-"${ID}"v1s1/func/meansub-"${ID}"v1s1_task-origrec1_run-04_bold_topup_stripped.nii.gz \
+
+	# DAY 2
+	mri_synthstrip \
 	-i /Users/yyi/Desktop/ENGRAMS/preproc/sub-"${ID}"v2s1/func/meansub-"${ID}"v2s1_task-origrec2_run-05_bold_topup.nii.gz \
 	-o /Users/yyi/Desktop/ENGRAMS/preproc/sub-"${ID}"v2s1/func/meansub-"${ID}"v2s1_task-origrec2_run-05_bold_topup_stripped.nii.gz
 
